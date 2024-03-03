f89d9a75d9be3bcacc3bfa6cb0188fb8f3b8007a
Lewis <lewiswalter192@gmail.com>
2024-03-04 00:31:29 +0300

    auth --google sign in success

--- app/src/main/java/com/licoding/uber/MainActivity.kt
+++ app/src/main/java/com/licoding/uber/MainActivity.kt
@@ -6,10 +6,11 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.licoding.uber.main.presentation.Home
 import com.licoding.uber.ui.theme.UberTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,7 +19,15 @@ class MainActivity : ComponentActivity() {
         setContent {
             UberTheme {
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = "home"
+                    ) {
+                        composable("home") {
+                            Home()
+                        }
+                    }
                 }
             }
         }
---
+++ app/src/main/java/com/licoding/uber/main/presentation/Home.kt
@@ -0,0 +1,9 @@
+package com.licoding.uber.main.presentation
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun Home() {
+    Text("Hey")
+}
